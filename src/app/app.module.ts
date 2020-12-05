cea3032830047aca7acbb8f62b2b87a28974f970
tonykobe8 <tonykobe8@gmail.com>
2020-12-05 13:38:24 +0200

    1 file modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ReferenceComponent } from './reference/reference.component';
 
 
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AboutComponent, ContactComponent],
+  declarations: [AppComponent, HomeComponent, AboutComponent, ContactComponent, EducationComponent, ExperienceComponent, ReferenceComponent],
  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule],
  
   bootstrap: [AppComponent]

03eecc9c8c748aea8d06020cd63f7234e4b910b1
tonykobe8 <tonykobe8@gmail.com>
2020-12-05 13:47:10 +0200

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,19 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ReferenceComponent } from './reference/reference.component';
 const routes: Routes = [
   { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
   {path:'' ,component:HomeComponent},
  {path:'home' ,component: HomeComponent},
 {path:'about' ,component: AboutComponent},
   {path:'contact' ,component: ContactComponent}
+  ,
+  {path:'education' ,component:EducationComponent},
+  {path:'experience' ,component: ExperienceComponent},
+  {path:'',component: ReferenceComponent}
 ];
 @NgModule({
   imports: [

30d49425f9ba9e900253f282030af8088189d468
tonykobe8 <tonykobe8@gmail.com>
2020-12-06 13:17:04 +0200

    2 files modified

--- src/app/reference/reference.component.ts
+++ src/app/reference/reference.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ReferenceComponent implements OnInit {
 
   constructor() { }
-
+reference =[{Name:'Nokwanda',sname:'Maranjane',tel:'+2783 555 1652',email:'nokwanda@mlab.co.za',position:'Facilitator'}];
+reference1 =[{Name:'Molokomme M.A',sname:'',tel:'015 505 0439 / 082 784 6889',email:'',position:'Principal and WIL Montor'}];
   ngOnInit() {
   }
 
